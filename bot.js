7b79dc90f64196a95c4660cef5b5d0eb8d162c1f
Lightence <30933088+Lightence@users.noreply.github.com>
2018-09-30 01:32:12 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.NDk1NTU1MzYwNDk3NTk4NDc1.DpFkUA.B0UMLJ-F6XamLnN2_dUAtu0NPKY);
